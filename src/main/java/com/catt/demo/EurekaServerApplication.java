cf662608022bb0b501d3d95515ed6c93940c0dad
hua <m15622201762@163.com>
2019-05-24 19:02:39 +0800

    demo

--- src/main/java/com/catt/demo/EurekaServerApplication.java
+++ src/main/java/com/catt/demo/EurekaServerApplication.java
@@ -2,12 +2,12 @@ package com.catt.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class DemoApplication {
-
+@EnableEurekaServer
+public class EurekaServerApplication {
   public static void main(String[] args) {
-    SpringApplication.run(DemoApplication.class, args);
+    SpringApplication.run(EurekaServerApplication.class, args);
   }
-
 }
